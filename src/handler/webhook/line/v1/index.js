14a75e1026ea72b8c8f38f0887c8ce1ea337df93
William AD <w.albertusd@gmail.com>
2018-04-06 20:06:25 +0700

    forget to complete handler

--- src/handler/webhook/line/v1/index.js
+++ src/handler/webhook/line/v1/index.js
@@ -2,7 +2,16 @@ const Bluebird = require('bluebird');
 const _ = require('lodash');
 const line = require('../../../../singleton/line');
 
-module.exports
+module.exports = (req, res) => {
+    Bluebird.all(_.map(req.body.events, handleEvent))
+        .then((result) => {
+            res.json(result);
+        })
+        .catch((err) => {
+            console.log(err);
+            return Bluebird.resolve(null);
+        })
+}
 
 const handleEvent = (event) => {
     if (_.isEqual(event.type, 'message')) {
